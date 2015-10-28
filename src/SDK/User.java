796a22a4cfde3af7a6aa33bd68ee0625e3822782
Hetu13ac <Hetu13ac@student.cbs.dk>
2015-10-28 11:08:34 +0100

    Initial commit

---
+++ src/SDK/User.java
@@ -0,0 +1,18 @@
+package SDK;
+
+/**
+ * Created by HenrikTuyen on 28/10/15.
+ */
+public class User {
+
+    private String username;
+    private String password;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/example/HelloWorldClient.java
@@ -0,0 +1,8 @@
+package example;/**
+ * Created by HenrikTuyen on 26/10/15.
+ */
+public class HelloWorldClient {
+  public static void main(String[] argv) {
+
+  }
+}
